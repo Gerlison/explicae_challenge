00c8c18b9b8d019b1df6686ac307af2818eca620
Francisco Gerlison <franciscojerlison1@gmail.com>
2020-10-23 08:54:04 -0300

    feat: apply module pattern

--- megasena/script.js
+++ megasena/script.js
@@ -1,83 +1,88 @@
-const tables = [];
+const megasena = (() => {
+  const _tables = [];
 
-const getUniqueRandomDozen = (arrayToVerify) => {
-  const newDozen = Math.ceil(Math.random() * 60);
-  if (!arrayToVerify.includes(newDozen)) return newDozen;
-  return getUniqueRandomDozen(arrayToVerify);
-};
-
-const getGameDozens = () => {
-  return Array.from({ length: 6 })
-    .reduce((acc) => {
-      acc.push(getUniqueRandomDozen(acc));
-      return acc;
-    }, [])
-    .sort();
-};
-
-const createTableFields = (tableElement) => {
-  let fieldNumber = 1;
-  const rows = Array.from({ length: 6 }, () => {
-    const newTRElement = document.createElement("tr");
-
-    const cells = Array.from({ length: 10 }, (_, index) => {
-      const newTDElement = document.createElement("td");
-      newTDElement.id = `cell-${fieldNumber + index}`;
-      newTDElement.append(fieldNumber + index);
-      return newTDElement;
-    });
-
-    fieldNumber += 10;
-    newTRElement.append(...cells);
-    return newTRElement;
-  });
-
-  tableElement.append(...rows);
-};
-
-const appendTableToList = (tableElement) => {
-  const tableId = `table-${tables.length}`;
-  tableElement.id = tableId;
-  tables.push({
-    id: tableId,
-    dozenSequence: getGameDozens(),
-  });
-};
-
-const createTable = () => {
-  const rootElement = document.querySelector("#root");
-  const newTableElement = document.createElement("table");
-  createTableFields(newTableElement);
-  appendTableToList(newTableElement);
-  rootElement.appendChild(newTableElement);
-};
-
-const checkDozens = (tableId) => {
-  const activateTableCells = (id) => (dozen) => {
-    document.querySelector(`#${id} #cell-${dozen}`).style.backgroundColor =
-      "#ff00ff";
+  const _getUniqueRandomDozen = (arrayToVerify) => {
+    const newDozen = Math.ceil(Math.random() * 60);
+    if (!arrayToVerify.includes(newDozen)) return newDozen;
+    return _getUniqueRandomDozen(arrayToVerify);
   };
 
-  if (tableId) {
-    return tables
-      .find(({ id }) => id === tableId)
-      .dozenSequence.forEach(activateTableCells(tableId));
-  }
+  const _createTableFields = (tableElement) => {
+    let fieldNumber = 1;
+    const rows = Array.from({ length: 6 }, () => {
+      const newTRElement = document.createElement("tr");
 
-  tables.forEach((table) => {
-    table.dozenSequence.forEach(activateTableCells(table.id));
-  });
-};
+      const cells = Array.from({ length: 10 }, (_, index) => {
+        const newTDElement = document.createElement("td");
+        newTDElement.id = `cell-${fieldNumber + index}`;
+        newTDElement.append(fieldNumber + index);
+        return newTDElement;
+      });
+
+      fieldNumber += 10;
+      newTRElement.append(...cells);
+      return newTRElement;
+    });
+
+    tableElement.append(...rows);
+  };
+
+  const _appendTableToList = (tableElement) => {
+    const tableId = `table-${_tables.length}`;
+    tableElement.id = tableId;
+    _tables.push({
+      id: tableId,
+      dozenSequence: generateGameDozens(),
+    });
+  };
+
+  const generateGameDozens = () => {
+    return Array.from({ length: 6 })
+      .reduce((acc) => {
+        acc.push(_getUniqueRandomDozen(acc));
+        return acc;
+      }, [])
+      .sort();
+  };
+
+  const createTable = () => {
+    const rootElement = document.querySelector("#root");
+    const newTableElement = document.createElement("table");
+    _createTableFields(newTableElement);
+    _appendTableToList(newTableElement);
+    rootElement.appendChild(newTableElement);
+  };
+
+  const hightlightSelectedDozens = () => {
+    const activateTableCells = (id) => (dozen) => {
+      document.querySelector(`#${id} #cell-${dozen}`).style.backgroundColor =
+        "#ff00ff";
+    };
+
+    _tables.forEach((table) => {
+      table.dozenSequence.forEach(activateTableCells(table.id));
+    });
+  };
+
+  return {
+    createTable,
+    hightlightSelectedDozens,
+    generateGameDozens,
+    get tables() {
+      return _tables;
+    },
+  };
+})();
 
 window.onload = () => {
-  createTable();
-  createTable();
-  createTable();
+  megasena.createTable();
+  megasena.createTable();
+  megasena.createTable();
 
-  checkDozens();
+  megasena.hightlightSelectedDozens();
 };
 
 const addTableManually = () => {
-  createTable();
-  checkDozens(tables[tables.length - 1].id);
+  megasena.createTable();
+  megasena.hightlightSelectedDozens();
 };
